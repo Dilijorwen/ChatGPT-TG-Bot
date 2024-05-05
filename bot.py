a141464d04bb34c8591009b2ef0a0674f8a309bb
Dilijorwen <Dirirjabl657@gmail.com>
2024-05-05 15:04:24 +1000

    Минимальный бот

--- bot.py
+++ bot.py
@@ -8,9 +8,14 @@ from dotenv import load_dotenv
 
 from aiogram import Bot, Dispatcher, html
 from aiogram.filters.command import CommandStart, Command
-from aiogram.types import Message
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.types import Message, KeyboardButton, KeyboardButtonRequestChat, KeyboardButtonPollType, KeyboardButtonRequestUser
+from aiogram.enums.dice_emoji import DiceEmoji
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 # Функция, которая подгружает ключ – значения из “.env
+
 load_dotenv()
 # Здесь мы присеваем переменной TOKEN значение, котрое он вытащил по ключу из “.env”.
 TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
@@ -23,16 +28,27 @@ dp = Dispatcher()
 async def command_start_handler(message: Message) -> None:
     # await - требование к вызову функции для асинхронки
     # После messeage стоит дополнение к функции, которое говорит, что бот сделает
+    # massage.from_user.full_name - Определяет имя пользователья
+    # html.bold - делает жирную разметку для имени пользователя в стиле html(для этого мы ниже делам parse_mode)
     await message.answer(f"Hello, {html.bold(message.from_user.full_name)}! \nНапиши /help")
 
 @dp.message(Command("help"))
 async def command_start_handler(message: Message) -> None:
-    await message.reply("Привет! Я тестовый бот для методички.\n"
-                         "Я умею повторять за тобой сообщения и пока все)))")
+    await message.reply("Привет! Я тестовый бот для методички\n"
+                         "У менять есть селудующие команды:\n"
+                        "/dice - мы с тобой кинем шестигранный кубик\n"
+                        "/help - выведу тебе данное сообщение\n"
+                        "А также могу повторять за тобой сообщения.")
 
+@dp.message(Command("dice"))
+async def dice_handler(message: Message):
+    await message.answer_dice(emoji=DiceEmoji.DICE)
+
+# Эта штука находиться внизу, потому что она принимает все сообщения, и если поставить ее наверх,
+# то она примет даже “/help” и просто его повторит и не выведет нужную инфу
 @dp.message()
 async def echo_handler(message: Message) -> None:
-    # Использум блок try-cacth
+    # Использум блок try...cacth
     try:
         await message.send_copy(chat_id=message.chat.id)
     except ValueError:
@@ -40,12 +56,13 @@ async def echo_handler(message: Message) -> None:
 
 
 async def main() -> None:
-    #Объявили бота с токеном
-    bot = Bot(token=TOKEN)
+    #Объявили бота с токеном c парс модом, который будет форматировать сообщения
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     #Начинаем полинг
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
+    # Логирование, оно нужно, чтобы мне не пропустили важные сообщения в консоли
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())

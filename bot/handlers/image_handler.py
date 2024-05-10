8114176198e6d9b8cc0c9b8959fa04ff73867e9b
Dilijorwen <Dirirjabl657@gmail.com>
2024-05-10 11:23:42 +1000

    Добавил функционал
    Теперь можете общаться с gpt и генерить картинки

---
+++ bot/handlers/image_handler.py
@@ -0,0 +1,91 @@
+import json
+import os
+
+import requests
+from dotenv import load_dotenv
+
+from aiogram import Router
+from aiogram.filters.command import Command
+from aiogram.types import Message, FSInputFile
+
+router = Router()
+
+load_dotenv()
+
+IMAGEAI_TOKEN = os.environ["IMAGEAI_TOKEN"]
+URL_IMAGEAI = os.environ["URL_IMAGEAI"]
+MODEL_IMAGEAI = os.environ["MODEL_IMAGEAI"]
+RESOLUTION_IMAGEAI = os.environ["RESOLUTION_IMAGEAI"]
+STYLE_IMAGEAI = os.environ["STYLE_IMAGEAI"]
+
+
+def text_to_image(text: str, id: int):
+    # Это  и есть общение с API
+    # Здесь даем ему наш токен в header, мы его не разбирали, но он похож на body
+    headers = {
+        "Authorization": f"Bearer {IMAGEAI_TOKEN}"}
+
+    # Ссылка API
+    url = f"{URL_IMAGEAI}"
+
+    # Наш body
+    payload = {
+        "providers": f"{MODEL_IMAGEAI}",
+        "text": f"{text}", # Это наш промт, то есть текст, по которому мы получим катинку. Его мы передаем в саму функцию
+        f"{MODEL_IMAGEAI}": f"{STYLE_IMAGEAI}",
+        "resolution": f"{RESOLUTION_IMAGEAI}",
+    }
+
+    # Получаем ответ на наш пост запрос
+    # request.post - это пост запрос по нашему API, мы отправляем ему body(payload) и headers
+    response = requests.post(url, json=payload, headers=headers)
+    # Мы получили на ша ответ в json формате, тоесть в формате словаря
+    result = json.loads(response.text)
+
+    # Это мы получаем нужныем нам данные, то есть ссылку на картинку
+    image_url = result[f'{MODEL_IMAGEAI}']['items'][0]['image_resource_url']
+    r = requests.get(image_url)
+
+
+
+    # Насильно создаст папку iamges в директории с ботом, нужно для docker
+    os.makedirs("images", exist_ok=True)
+
+    # Cохранит картинку в нужную папку
+    with open(f'images/{id}.png', 'wb') as file:
+        file.write(r.content)
+
+@router.message(Command("image"))
+async def command_image_handler(message: Message):
+    # Сохраняем сообщение, чтобы его потом удалить
+    # Я не знаю как точно это работает,
+    # но скорее всего он сохраняет id сообщения и чата в эту переменную
+    edited = await message.reply('Генерируется картинка️️🏞')
+
+    # Мы пишем ему /image {текст},
+    # эта строка убирает image и оставляет текст
+    # Бот не видит этот "/", он нужен для активации команды
+    text = message.text[6:]
+
+    # Сохраняем  id для нейминга файла
+    id = message.chat.id
+
+    # Вызываем функцию
+    text_to_image(text, id)
+
+
+    try:
+        await message.reply_photo(photo=FSInputFile(f"images/{id}.png"))
+        # Ловит все ошибки
+        # Так нельзя, лучше ловить все ошибки отдельными ошибками,
+        # чтобы пользователь понимал точно в чем ошибка
+        # ы ее сохраняем как перемнную "e"
+    except Exception as e:
+        await message.reply("Мне не удалось ответить на вопрос🥺Попробуйте позже\n"
+                            "Возможные причины: \n"
+                            "1) Токен более недействителен\n"
+                            "2) У токена закончились деньги\n"
+                            "3) Может быть сервер плохо сработал")
+        print(e) # Выведем ошибку в консоль для понимания, что случилось,
+        # чтобы исправить
+    await edited.delete()
--- bot/telegram.py
+++ bot/telegram.py
@@ -9,27 +9,27 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from handlers import help_handler, dice_handler, start_handler
+from handlers import help_handler, dice_handler, start_handler, image_handler, gpt_handler
 
 
 async def main() -> None:
-    # Функция, которая подгружает ключ – значения из “.env
+    # Функция, которая подгружает ключ – значения из ".env"
     load_dotenv()
 
-    # Здесь мы присеваем переменной TELEGRAM_BOT_TOKEN значение, котрое он вытащил по ключу из “.env”.
+    # Здесь мы присеваем переменной TELEGRAM_BOT_TOKEN значение, которое он вытащил по ключу из “.env”.
     TELEGRAM_BOT_TOKEN = os.environ["TELEGRAM_BOT_TOKEN"]
 
     # Объявляем диспечер
     dp = Dispatcher()
 
     # Добовляем в наш диспечер роутеры
-    dp.include_routers(start_handler.router, help_handler.router, dice_handler.router)
+    dp.include_routers(start_handler.router, help_handler.router, dice_handler.router,
+                       image_handler.router, gpt_handler.router)
 
-
-    #Объявили бота с токеном c парс модом, который будет форматировать сообщения
+    # Объявили бота с токеном c парс модом, который будет форматировать сообщения
     bot = Bot(token=TELEGRAM_BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
-    #Начинаем полинг
+    # Начинаем полинг
     await dp.start_polling(bot)
 
 
